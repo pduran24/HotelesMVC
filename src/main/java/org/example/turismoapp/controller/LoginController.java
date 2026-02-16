694cbd593b1026c9d9d6e374310afed8b35dcc20
Pablo <pabloduranp5@gmail.com>
2026-02-16 09:07:13 +0100

    añade login funcional

---
+++ src/main/java/org/example/turismoapp/controller/LoginController.java
@@ -0,0 +1,15 @@
+package org.example.turismoapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+class LoginController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
